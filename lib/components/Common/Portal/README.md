e5050f3982c34409f2504596d9ffe5e94cd21cc5
dongxu.shelton <dongxu.shelton@bytedance.com>
2018-01-23 15:21:36 +0800

    issue: Cannot resolve module
    Change-Id: I0cc406844c2970b47e387aafd21f3c8b34b82afb

---
+++ lib/components/Common/Portal/README.md
@@ -0,0 +1,10 @@
+portal
+```javascript
+<div>hello</div>
+```
+```typescript
+const a
+```
+```javascript
+<div>world</div>
+```
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,10 +1,27 @@
 module.exports = {
 	title: 'React Style Guide Example',
 	defaultExample: true,
-	components: './lib/components/**/[A-Z]*.js',
+	sections: [
+		{
+			name: 'Components',
+			content: './styleguide/CNAME',
+			sections: [
+				{
+					name: 'Common',
+					components: './lib/components/Common/**/index.tsx',
+				}
+			]
+		}
+	],
+	propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
 	webpackConfig: {
 		module: {
 			loaders: [
+				{
+					test: /\.(ts|tsx)?$/,
+					exclude: /node_modules/,
+					loader: 'ts-loader',
+				},
 				{
 					test: /\.jsx?$/,
 					exclude: /node_modules/,
